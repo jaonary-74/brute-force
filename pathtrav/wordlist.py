f64370e75bdf0839b835fb49640f78bab3b6bb9a
jaonary-74 <jaonary74@gmail.com>
2024-03-19 19:02:48 +0300

    fix: add changes

--- pathtrav/wordlist.py
+++ pathtrav/wordlist.py
@@ -2,48 +2,46 @@ import requests
 import threading
 
 def make_request(url):
-    # Make an HTTP GET request to the given URL
+    # Sends a GET request to the specified URL and returns the response's status code
     response = requests.get(url)
     return response.status_code
 
 def thread_function(word_list, base_url):
-    # Iterate through the word list assigned to the current thread
+    # Processes a list of words and checks each word by making a request to the base URL
     for word in word_list:
-        # Construct the complete URL by appending the word to the base URL
         url = base_url + word
-        # Make the HTTP request and get the response code
         response_code = make_request(url)
-        # Check if the response code indicates a successful request (200 or 500)
         if response_code == 200 or response_code == 500:
-            # Print the URL and response code if a successful response is received
+            # Prints the URL and status code if the response is either 200 (OK) or 500 (Internal Server Error)
             print(f"Found: {url} (Status Code: {response_code})")
 
 def main():
+    # Entry point of the program
     base_url = "http://127.0.0.1:5000/"
     word_list = []
 
-    # Read the word list from the file
+    # Reads a list of words from a file
     with open("dir_list.txt", "r") as file:
         word_list = file.read().splitlines()
 
+    # Configures the number of threads and words per thread
     num_threads = 3
     word_list_size = len(word_list)
     words_per_thread = word_list_size // num_threads
 
     threads = []
+
+    # Spawns multiple threads and assigns words to each thread for processing
     for i in range(num_threads):
         start_index = i * words_per_thread
         end_index = (i + 1) * words_per_thread if i < num_threads - 1 else word_list_size
         thread_words = word_list[start_index:end_index]
 
-        # Create a new thread, passing the assigned word list and the base URL
         thread = threading.Thread(target=thread_function, args=(thread_words, base_url))
-        # Start the thread
         thread.start()
-        # Add the thread to the list of active threads
         threads.append(thread)
 
-    # Wait for all threads to complete
+    # Waits for all threads to finish before exiting the program
     for thread in threads:
         thread.join()
 
